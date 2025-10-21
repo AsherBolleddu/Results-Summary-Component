32f9f40fc439cf950f8bad5270e1c781b95d0baa
Asher Bolleddu <104097139+AsherBolleddu@users.noreply.github.com>
2025-10-21 12:45:30 -0500

    added main

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import data from "./data.json";
 function App() {
   return (
     <div className="bg-white md:bg-blue-50 md:min-h-screen md:grid md:place-items-center">
-      <div className="bg-white flex flex-col gap-6 md:gap-0 md:flex-row md:rounded-4xl md:shadow-[0_30px_60px_0_rgba(61,108,236,0.15)] md:overflow-hidden pb-[1.875rem] md:pb-0">
+      <main className="bg-white flex flex-col gap-6 md:gap-0 md:flex-row md:rounded-4xl md:shadow-[0_30px_60px_0_rgba(61,108,236,0.15)] md:overflow-hidden pb-[1.875rem] md:pb-0">
         <ResultCard />
         <SummaryCard data={data} />
-      </div>
+      </main>
     </div>
   );
 }
